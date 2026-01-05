4cc529521d2c696227d32cc7e41aaf0ca124f153
Brannon Davidson <142679278+Pssmst@users.noreply.github.com>
2026-01-05 13:24:48 -0600

    keep .gitkeep in assets/Music

--- update-music-folder.bat
+++ update-music-folder.bat
@@ -1,3 +1,7 @@
 @echo off
-robocopy "C:\Users\Owner\Music" "%~dp0\App\assets\Music" /MIR /MT:16 /XD "FlashIntegro" "Covers (Images)" "iTunes" "Playlists"
+robocopy "C:\Users\Owner\Music" "%~dp0\App\assets\Music" ^
+    /MIR ^
+    /MT:16 ^
+    /XD "FlashIntegro" "Covers (Images)" "iTunes" "Playlists" ^
+    /XF ".gitkeep" ^
 pause
